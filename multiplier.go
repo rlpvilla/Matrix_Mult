518b5ec7533d2ac462e78978573a71d36107d7e2
rlpvilla <rlvilla@ucsc.edu>
2013-10-10 17:29:17 -0700

    Progressing
    Requires user session or query string implementation, as well as testing
    for multiplication and validity

--- multiplier.go
+++ multiplier.go
@@ -2,30 +2,88 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"net/http"
 	"html/template"
 	//"regexp"
 )
 
-type matrix struct {
-	id int
-	Rows int
-	Columns int
+type matrixArrangement struct {
+	ARows int
+	AColumns int
+	BColumns int
 }
 
-func renderTemplate (w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate (w, "templates" + tmpl + ".html", p)
+/*
+func arrangeMatrices (arows int, acolumns int, bcolumns int) (*matrixArrangement) {
+	m := &matrixArrangement{ARows: arows, AColumns: acolumns, BColumns: bcolumns};
+	return m;
+}
+*/
+
+var templates = template.Must (template.ParseFiles ("index.html"))
+
+func renderTemplate (w http.ResponseWriter, tmpl string, m *matrixArrangement) {
+	err := templates.ExecuteTemplate (w, tmpl + ".html", m)
     if err != nil {
     	http.Error (w, err.Error (), http.StatusInternalServerError)
         return
     }
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+func valueConvert(val string) (int, error) {
+	value, err := strconv.Atoi (val);
+	if err != nil {
+		fmt.Println("A conversion error occurred: %s", err);
+		return 0, err
+	}
+
+	return value, nil
+}
+
+func (m *matrixArrangement) viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("View");
+
+	init := "index";
 	
+	renderTemplate (w, init, m);
+}
+
+func (m *matrixArrangement) defineHandler(w http.ResponseWriter, r *http.Request) {
+	a, err1 := valueConvert(r.FormValue("aRows"));
+	
+	if err1 != nil {
+		http.Redirect(w, r, "/", http.StatusFound);
+		fmt.Println("Please evaluate input for errors and resubmit");
+		return
+	}
+	
+	b, err2 := valueConvert(r.FormValue("aColumns"));
+
+	if err2 != nil {
+		http.Redirect(w, r, "/", http.StatusFound);
+		fmt.Println("Please evaluate input for errors and resubmit");
+		return
+	}
+
+	c, err3 := valueConvert(r.FormValue("bColumns"));
+
+	if err3 != nil {
+		http.Redirect(w, r, "/", http.StatusFound);
+		fmt.Println("Please evaluate input for errors and resubmit");
+		return
+	}
+
+	m.ARows = a;
+	m.AColumns = b;
+	m.BColumns = c;
+
+	m.viewHandler(w, r)
 }
 
 func main() {
-	http.HandleFunc("/", rootHandler)
+	Init := &matrixArrangement {ARows: 0, AColumns: 0, BColumns: 0}
+	http.HandleFunc("/", Init.viewHandler)
+	http.HandleFunc("/define", Init.defineHandler)
 	http.ListenAndServe(":8080", nil)
 }
